4ca0a833df6edcb349f9c29c96df216cfd5dcb4f
Louise Crow <louise.crow@gmail.com>
2016-10-17 13:55:02 +0100

    Add static demo pages.

---
+++ app/controllers/alaveteli_professional_controller.rb
@@ -0,0 +1,14 @@
+# -*- encoding : utf-8 -*-
+class AlaveteliProfessionalController < ApplicationController
+
+  def login
+  end
+
+  def frontpage
+    render :frontpage, :layout => false
+  end
+
+  def find_auth
+  end
+
+end
