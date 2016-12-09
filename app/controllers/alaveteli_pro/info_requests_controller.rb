5c5539c3d3c35eb5f5619ac4fac2a132c1b1ea05
Steven Day <steve@mysociety.org>
2016-12-09 16:09:27 +0000

    Add a controller action for updating pro info requests' status

--- app/controllers/alaveteli_pro/info_requests_controller.rb
+++ app/controllers/alaveteli_pro/info_requests_controller.rb
@@ -37,6 +37,16 @@ class AlaveteliPro::InfoRequestsController < AlaveteliPro::BaseController
     end
   end
 
+  def update
+    @info_request = InfoRequest.find(params[:id])
+    authorize! :update_request_state, @info_request
+    new_status = info_request_params[:described_state]
+    @info_request.set_described_state(new_status, current_user)
+    flash[:notice] = _("Your request has been updated!")
+    redirect_to show_alaveteli_pro_request_path(
+      url_title: @info_request.url_title)
+  end
+
   private
 
   def show_errors
@@ -92,5 +102,9 @@ class AlaveteliPro::InfoRequestsController < AlaveteliPro::BaseController
         mail_message.message_id
       )
     end
+  end
+
+  def info_request_params
+    params.require(:info_request).permit(:described_state)
   end
 end
